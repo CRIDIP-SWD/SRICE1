8d92c2fc98baf1e5f8eb66bb07aad8a2f6e897ea
CRIDIP-SWD <mmockelyn@cridip.com>
2015-11-25 17:50:46 +0100

    SRICE1

--- core/classe/salarie.php
+++ core/classe/salarie.php
@@ -11,14 +11,14 @@ class salarie
     public function count_salarie()
     {
         $sql = mysql_query("SELECT COUNT(*) FROM salarie")or die(mysql_error());
-        $data = mysql_fetch_array($sql);
+        $data = mysql_result($sql,0);
         return $data;
     }
 
     public function count_ad()
     {
         $sql = mysql_query("SELECT COUNT(*) FROM ayant_droit")or die(mysql_error());
-        $data = mysql_fetch_array($sql);
+        $data = mysql_result($sql,0);
         return $data;
     }
 }
