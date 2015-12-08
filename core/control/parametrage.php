e08983ff527166113c694bce13a4ff5e99420501
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-08 18:46:17 +0100

    SRICE

--- core/control/parametrage.php
+++ core/control/parametrage.php
@@ -4,6 +4,6 @@ if(isset($_GET['action']) && $_GET['action'] == 'new-save-bdd')
     include "../config.php";
     include "../classe.php";
 
-    $dump = new \Ifsnop\Mysqldump\Mysqldump('srice', 'root', '1992maxime');
+    $dump = new \Ifsnop\Mysqldump\Mysqldump("mysql:host=localhost;dbname=srice", "root", "1992maxime");
     $dump->start(dirname(__DIR__)."/test.sql");
 }
