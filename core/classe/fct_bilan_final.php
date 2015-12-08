5d1d25a4f449c7d4493aa0a56780cc14af991f42
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-08 11:01:06 +0100

    SRICE1

--- core/classe/fct_bilan_final.php
+++ core/classe/fct_bilan_final.php
@@ -8,12 +8,5 @@
  */
 class fct_bilan_final
 {
-    public function cooerance()
-    {
-        $sql1 = mysql_query("SELECT SUM(montant) FROM compta_bilan_actif")or die(mysql_error());
-        $sql2 = mysql_query("SELECT SUM(montant) FROM compta_bilan_passif")or die(mysql_error());
-        $sql3 = mysql_query("SELECT SUM(debit) FROM compta_resultat")or die(mysql_error());
-        $sql4 = mysql_query("SELECT SUM(credit) FROM compta_resultat")or die(mysql_error());
-        $data1 = m
-    }
+
 }
