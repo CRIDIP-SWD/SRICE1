ea99476186fd45219bef8fae2ac27976ae28b5db
CRIDIP-SWD <mmockelyn@cridip.com>
2015-12-09 15:46:55 +0100

    SRICE

--- view/index.php
+++ view/index.php
@@ -412,13 +412,14 @@
                                                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                                 <h3 class="modal-title"><i class="fa fa-user-md"></i> Ajout d'un utilisateur</h3>
                                             </div>
-                                            <div class="modal-body">
-                                                <div class="row">
-                                                    <div class="col-md-12">
+                                            <form class="form-horizontal" action="" method="post">
+                                                <div class="modal-body">
 
-                                                    </div>
                                                 </div>
-                                            </div>
+                                                <div class="modal-footer">
+
+                                                </div>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
